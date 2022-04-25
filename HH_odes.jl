e47f1aaef57f055887644f59ca1655b65662a8f6
Raphael Schmetterling <rjzs2@cam.ac.uk>
2022-04-25 15:53:37 +0100

    Added s algorithm, but only s_hat converges.

--- HH_odes.jl
+++ HH_odes.jl
@@ -39,6 +39,11 @@ function gating_n(v, r=-53.)
     return τ, σ
 end
 
+# Dynamics of synaptic filter
+function dsyn(v, s)
+    ds = 0.53*(1/(1+exp(-(v-2)/5)))*(1-s)-0.18*s
+end
+
 function HH_ode!(dz,z,p,t)
     Iapp =          p[1]
     c =             p[2]
@@ -130,4 +135,75 @@ function HH_observer!(dz,z,p,t)
     dP = (dP+dP')/2;
 
     dz[:] = [dv;dm;dh;dn;dv̂;dm̂;dĥ;dn̂;dθ̂;dP[:];dΨ]';
+end
+
+function HH_s_observer!(dz,z,p,t)
+    Iapp =          p[1]
+    c =             p[2]
+    (gNa,gK,gL) =   p[3]
+    (ENa,EK,EL) =   p[4]
+    (α,γ) =         p[5]
+    (rm, rh, rn) =  p[6]
+
+    # True system
+    v = z[1]
+    m = z[2]
+    h = z[3]
+    n = z[4]
+    s = z[72]
+
+    (τm,σm) = gating_m(v);
+    (τh,σh) = gating_h(v);
+    (τn,σn) = gating_n(v);
+
+    θ = 1/c*[gNa gK gL gNa*ENa gK*EK gL*EL 1]
+    ϕ = [-m^3*h*v ...
+         -n^4*v ... 
+         -v ...
+         m^3*h ...
+         n^4 ...
+         1 ...
+         Iapp(t)];
+
+    dv = dot(ϕ,θ) - s
+    dm = 1/τm*(-m + σm);
+    dh = 1/τh*(-h + σh);
+    dn = 1/τn*(-n + σn);
+    ds = dsyn(v, s)
+
+    # Adaptive observer
+    v̂ = z[5]
+    m̂ = z[6]
+    ĥ = z[7]
+    n̂ = z[8]
+    θ̂ = z[9:15]
+    P = reshape(z[15+1:15+49],7,7);    
+    P = (P+P')/2
+    Ψ = z[15+49+1:15+49+7]
+    s_hat = z[73]
+
+    (τm̂,σm̂) = gating_m(v, rm);
+    (τĥ,σĥ) = gating_h(v, rh);
+    (τn̂,σn̂) = gating_n(v, rn);
+
+    ϕ̂ = [-m̂^3*ĥ*v ...
+         -n̂^4*v ... 
+         -v ...
+         m̂^3*ĥ ...
+         n̂^4 ...
+         1 ...
+         Iapp(t)];
+
+    dv̂ = dot(ϕ̂,θ̂) - s_hat
+    dm̂ = 1/τm̂*(-m̂ + σm̂);
+    dĥ = 1/τĥ*(-ĥ + σĥ);
+    dn̂ = 1/τn̂*(-n̂ + σn̂);
+    ds_hat = dsyn(v, s_hat) + γ*(1+Ψ'*P*Ψ)*(s-s_hat)
+
+    dθ̂ = γ*P*Ψ*(s-s_hat);
+    dΨ = -γ*Ψ + ϕ̂; 
+    dP = α*P - ((P*Ψ)*(P*Ψ)');
+    dP = (dP+dP')/2;
+
+    dz[:] = [dv;dm;dh;dn;dv̂;dm̂;dĥ;dn̂;dθ̂;dP[:];dΨ;ds;ds_hat]';
 end
