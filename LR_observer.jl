14f8dfc1626e5824f8e82d10a08c3fc0f95623d7
Raphael Schmetterling <rjzs2@cam.ac.uk>
2022-05-13 13:18:06 +0100

    Luka's circuit ready to test.

---
+++ LR_observer.jl
@@ -0,0 +1,41 @@
+using Plots
+using DifferentialEquations, LinearAlgebra
+
+include("LR_odes.jl")
+
+
+## Definition of parameters
+tau_s = 50
+tau_us = 50*50
+
+afn = -2
+asp = 2
+asn = -1.5
+ausp = 1.5
+
+dfn = 0
+dsp = 0
+dsn = -1.5
+dusp = -1.5
+
+# Initial conditions
+u0 = [0 0 0]
+
+Tfinal= 3000.0
+tspan=(0.0,Tfinal)
+
+Iapp = -2.
+
+# Parameter vector for simulations
+p=(afn,asp,asn,ausp,dfn,dsp,dsn,dusp,tau_s,tau_us,Iapp)
+
+# Simulation
+# Using the calcium observer
+prob = ODEProblem(LR_ODE!,u0,tspan,p) # Simulation without noise (ODE)
+sol = solve(prob,dtmax=0.1)
+
+
+## Generation of figures 
+# Voltage response
+p1=plot(sol.t, sol[1,:],linewidth=1.5,legend=false)
+ylabel!("V")
---
+++ LR_odes.jl
@@ -0,0 +1,45 @@
+# The model introduced in "Neuromodulation of Neuromorphic Circuits"
+# by Luka Ribar.
+
+using DifferentialEquations
+
+function sigmoid(x, k=1)
+    1 ./ (1 .+ exp(-k * x))
+end
+
+function element(V, a, delta)
+    a .* tanh.(V .- delta)
+end
+
+function LR_ODE!(du,u,p,t)
+    # Gains
+    afn = p[1]
+    asp = p[2]
+    asn = p[3]
+    ausp = p[4]
+
+    # Offsets
+    dfn = p[5]
+    dsp = p[6]
+    dsn = p[7]
+    dusp = p[8]
+
+    # Time constants
+    tau_s = p[9]
+    tau_us = p[10]
+
+    # Input
+    Iapp = p[11]
+
+    # Variables
+    V = u[1]
+    Vs = u[2]
+    Vus = u[3]
+
+    # ODEs
+    du[1] = -V  -element(V,afn,dfn) -element(Vs,asp,dsp)
+                -element(Vs,asn,dsn) -element(Vus,ausp,dusp)
+                +Iapp
+    du[2] = (1/tau_s)  * (V - Vs)
+    du[3] = (1/tau_us) * (V - Vus)
+end
