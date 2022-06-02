c3713fd1e0ffa38cb9c2561657ddb20f7a7101ca
Raphael Schmetterling <rjzs2@cam.ac.uk>
2022-06-02 12:18:26 +0100

    Distributed observer.

---
+++ GD_observer_dist.jl
@@ -0,0 +1,171 @@
+using Plots, JLD
+using DifferentialEquations, LinearAlgebra
+using Random, Distributions
+# Random.seed!(123)
+
+include("GD_odes_dist.jl")
+
+## Constant simulation parameters
+
+## Definition of reversal potential values. 
+const VNa = 40.; # Sodium reversal potential
+const VK = -90.; # Potassium reversal potential
+const VCa = 120.; # Calcium reversal potential
+const VH= -40.; # Reversal potential for the H-current (permeable to both sodium and potassium ions)
+const Vl = -50.; # Reversal potential of leak channels
+
+const C=0.1; # Membrane capacitance
+αCa=0.1; # Calcium dynamics (L-current)
+β=0.04 # Calcium dynamics (T-current)
+
+gl=0.3; # Leak current maximal conductance
+gNa=100.; # Sodium current maximal conductance
+gKd=65.; # Delayed-rectifier potassium current maximal conductance
+gAf=0.; # Fast A-type potassium current maximal conductance
+gAs=0.; # Slow A-type potassium current maximal conductance
+gKCa=8.; # Calcium-activated potassium current maximal conductance
+gCaL=4.; # L-type calcium current maximal conductance
+gCaT=0.5; # T-type calcium current maximal conductance
+gH=0.; # H-current maximal conductance
+
+# Observer parameters
+α1 = 0.008
+γ0 = 5 # 0.02
+γs = 5*ones(5) # 0.02*ones(5)
+
+# Modelling errors
+# True values are (45, 60, 85) for (mCaL, mCaT, hCaT)
+# The gates are 
+# (mNa, hNa, mKd, mAf, hAf, mAs, hAs, 
+# mCaL, mCaT, hCaT, mH). The true values are
+# (25, 40, 15, 80, 60, 60, 20, 45, 60, 85, 85, -30)
+err = 0.00 # Maximum proportional error in observer model. Try eg 0.05 and 0.1.
+# half_acts = (x_sample(45, err),x_sample(60, err),x_sample(85, err))
+half_acts = (25*(1+err),40*(1+err),15*(1-err),
+80*(1+err),60*(1-err),60*(1-err),
+20*(1+err),
+45*(1+err),60*(1-err),85*(1+err),85*(1+err),-30*(1-err))
+# TODO: Try random error. Try error in other params.
+
+# Errors in tau half act
+# mNa 100 hNa 50 mKd 30
+# mAf 100 hAf 100 mAs hAs 100 mCaL mCaT hCaT 30
+# mH 30
+err_t = 0.
+half_act_taus = (100*(1+err_t),50*(1-err_t),30*(1-err_t),
+            100*(1-err_t),100*(1+err_t),100*(1+err_t),100*(1+err_t),
+            30*(1+err_t),30*(1+err_t),30*(1-err_t),30*(1+err_t))
+
+# Initial conditions
+x₀ = init_neur(-70.);
+x̂₀ = [-60 0.4 0.4 0.4 0.4 0.4 0.5 0.3 0.5 0.6 0.1 0.5 0.2];
+θ̂₀ = [.1 .1 .1 .1 .1];
+P₀ = ones(5)';
+Ψ₀ = [0 0 0 0 0];
+u0 = [x₀ x̂₀ θ̂₀ P₀ Ψ₀]' # Let's feed in a column vector.
+
+Tfinal= 2000.0 # 14500.0
+tspan=(0.0,Tfinal)
+
+## Input current defition
+# Constant current
+#Iapp= 4. # Overwritten in the function by a hardcoded input.
+
+# Noise-generated current
+d = Normal(0,1)
+n_per_t = 5
+n = rand(d, Int(Tfinal*n_per_t)+2)
+# Iapp = t -> -1 - 0*t 
+
+# Interpolated noisy input for ODE solver
+function noisy_input(Iconst, noise, n_per_t, t)
+    dt = 1/n_per_t
+    y0j = Int.(floor.(t.*n_per_t)).+1
+    y0 = Iconst .+ noise[y0j]
+    y1 = Iconst .+ noise[y0j.+1]
+    t0 = t .- t.%dt
+    t1 = t0 .+ dt
+
+    y = y0 .+ (t .- t0).*(y1.-y0)./(t1-t0)
+end
+# nts = noisy_input(4,n,n_per_t,ts) # For LaTeXStrings
+
+Iconst = -1.5
+Iapp = t -> noisy_input(Iconst, n, n_per_t, t)
+save("data.jld","noise",n,"n_per_t",n_per_t)
+Iapp = t -> 4.
+
+# Current pulses
+I1=0. # Amplitude of first pulse
+ti1=300 # Starting time of first pulse
+tf1=302 # Ending time of first pulse
+I2=0. # Amplitude of second pulse
+ti2=350 # Starting time of second pulse
+tf2=370 # Ending time of first pulse
+
+# Have hardcoded the input current into the ODE fn.
+# # High frequency noise input 
+# Isines = .5*sin.(0.01*sol.t)+0.5*sin.(0.05*sol.t)
+
+## Current-clamp experiment
+# Parameter vector for simulations
+p=(Iapp,I1,I2,ti1,tf1,ti2,tf2,
+gNa,gKd,gAf,gAs,gKCa,gCaL,gCaT,gH,gl,half_acts,half_act_taus,α1)
+
+# Simulation
+# Using the calcium observer
+prob = ODEProblem(CBM_v_dist_observer!,u0,tspan,p) # Simulation without noise (ODE)
+# NOTE the above function is currently not using Ca, it's using Cah!!
+
+# prob = ODEProblem(CBM_observer!,u0,tspan,p) # Simulation without noise (ODE)
+
+sol = solve(prob,dtmax=0.1,saveat=0.1)
+# sol = solve(prob,alg_hints=[:stiff],reltol=1e-8,abstol=1e-8)
+# sol = solve(prob,AutoTsit5(Rosenbrock23()))
+# using LSODA
+# sol = solve(prob,lsoda(),reltol=1e-10,abstol=1e-10)
+
+# Alternative from Thiago:
+# sol = solve(prob,AutoTsit5(Rosenbrock23()),saveat=0.1)#,reltol=1e-8,abstol=1e-8
+
+# Extract output variables
+t = sol.t; Vref = sol[1,:]; Vh = sol[14,:]; Caref = sol[13,:]; Cah = sol[26,:]
+gLh = sol[30,:]; gTh = sol[31,:]
+
+## Generation of figures 
+# Voltage response
+p1=plot(sol.t, sol[1,:],linewidth=1.5,legend=false)
+ylabel!("V")
+
+p1b = plot(sol.t,sol[1,:])
+plot!(sol.t, sol[14,:])
+
+# Ca versus its estimate
+# i = 220000
+# j = lastindex(sol[1,:])
+p2 = plot(sol.t, sol[13,:])
+plot!(sol.t, sol[26,:])
+
+# Parameter estimates
+p3 = plot(sol.t,sol[27,:]) # gCaL
+p4 = plot(sol.t,sol[28,:]) # gCaT
+
+# Truncated figures
+j = size(sol)[3]
+i = round(Int,3*j/5)
+p1t = plot(t[i:j],Vref[i:j],legend=false)
+plot!(t[i:j],Vh[i:j])
+ylabel!("V")
+
+p2t = plot(t[i:j], Caref[i:j])
+plot!(t[i:j], Cah[i:j])
+
+# Parameter estimates
+p3t = plot(t[i:j],gLh[i:j]) # gCaL
+p4t = plot(t[i:j],gTh[i:j]) # gCaT
+
+p1b
+
+println("Means")
+println(mean(gLh[i:j]))
+println(mean(gTh[i:j]))
---
+++ GD_odes_dist.jl
@@ -0,0 +1,136 @@
+using DifferentialEquations, Plots, Plots.PlotMeasures, LaTeXStrings
+
+function CBM_v_dist_observer!(du,u,p,t)
+    # Stimulations parameters
+    Iapp=p[1] # Amplitude of constant applied current
+    I1=p[2] # Amplitude of first step input
+    I2=p[3] # Amplitude of second step input
+    ti1=p[4] # Starting time of first step input
+    tf1=p[5] # Ending time of first step input
+    ti2=p[6] # Starting time of second step input
+    tf2=p[7] # Ending time of second step input
+    
+    # Maximal conductances
+    gNa=p[8] # Sodium current maximal conductance
+    gKd=p[9]  # Delayed-rectifier potassium current maximal conductance
+    gAf=p[10] # Fast A-type potassium current maximal conductance
+    gAs=p[11] # Slow A-type potassium current maximal conductance
+    gKCa=p[12] # Calcium-activated potassium current maximal conductance
+    gCaL=p[13] # L-type calcium current maximal conductance
+    gCaT=p[14] # T-type calcium current maximal conductance
+    gH=p[15] # H-current maximal conductance
+    gl=p[16] # Leak current maximal conductance
+    
+    half_acts = p[17]
+    ha_ts = p[18] # Half_act taus (for observer model error)
+    α1 = p[19] # Alpha during first prat of simulation
+
+    # Variables
+    V=u[1] # Membrane potential
+    mNa=u[2] # Sodium current activation
+    hNa=u[3] # Sodium current inactivation
+    mKd=u[4] # Delayed-rectifier potassium current activation
+    mAf=u[5] # Fast A-type potassium current activation
+    hAf=u[6] # Fast A-type potassium current inactivation
+    mAs=u[7] # Slow A-type potassium current activation
+    hAs=u[8] # Slow A-type potassium current inactivation
+    mCaL=u[9] # L-type calcium current activation
+    mCaT=u[10] # T-type calcium current activation
+    hCaT=u[11] # T-type calcium current inactivation
+    mH=u[12] # H current activation
+    Ca=u[13] # Intracellular calcium concentration
+    
+    θ = [gCaL gCaT]
+    ϕ = 1/C*[-mCaL*(V-VCa) ...
+            -mCaT*hCaT*(V-VCa)];
+        
+                # Sodium current
+    b = (1/C) * (-gNa*mNa*hNa*(V-VNa) +
+                # Potassium Currents
+                -gKd*mKd*(V-VK) -gAf*mAf*hAf*(V-VK) -gAs*mAs*hAs*(V-VK) +
+                -gKCa*mKCainf(Ca)*(V-VK) +
+                # Cation current
+                -gH*mH*(V-VH) +
+                # Passive currents
+                -gl*(V-Vl) +
+                # Stimulation currents
+                +Iapp(t) + I1*pulse(t,ti1,tf1) + I2*pulse(t,ti2,tf2))
+    du[1] = dot(ϕ,θ) + b
+
+    # Internal dynamics
+    du[2] = (1/tau_mNa(V)) * (mNainf(V) - mNa)
+    du[3] = (1/tau_hNa(V)) * (hNainf(V) - hNa)
+    du[4] = (1/tau_mKd(V)) * (mKdinf(V) - mKd)
+    du[5] = (1/tau_mAf(V)) * (mAfinf(V) - mAf)
+    du[6] = (1/tau_hAf(V)) * (hAfinf(V) - hAf)
+    du[7] = (1/tau_mAs(V)) * (mAsinf(V) - mAs)
+    du[8] = (1/tau_hAs(V)) * (hAsinf(V) - hAs)
+    du[9] = (1/tau_mCaL(V)) * (mCaLinf(V) - mCaL)
+    du[10] = (1/tau_mCaT(V)) * (mCaTinf(V) - mCaT)
+    du[11] = (1/tau_hCaT(V)) * (hCaTinf(V) - hCaT)
+    du[12] = (1/tau_mH(V)) * (mHinf(V) - mH)
+    du[13] = (1/tau_Ca) * ((-αCa*gCaL*mCaL*(V-VCa))+(-β*gCaT*mCaT*hCaT*(V-VCa)) - Ca) 
+
+    # Adaptive observer
+    Vh=u[14] # Membrane potential
+    mNah=u[15] # Sodium current activation
+    hNah=u[16] # Sodium current inactivation
+    mKdh=u[17] # Delayed-rectifier potassium current activation
+    mAfh=u[18] # Fast A-type potassium current activation
+    hAfh=u[19] # Fast A-type potassium current inactivation
+    mAsh=u[20] # Slow A-type potassium current activation
+    hAsh=u[21] # Slow A-type potassium current inactivation
+    mCaLh=u[22] # L-type calcium current activation
+    mCaTh=u[23] # T-type calcium current activation
+    hCaTh=u[24] # T-type calcium current inactivation
+    mHh=u[25] # H current activation
+    Cah=u[26] # Intracellular calcium concentration
+    θ̂= u[27:31]
+    P = u[31+1:31+5]
+    Ψ = u[31+5+1:31+5+5]
+
+    if t < 5000
+        α = α1
+    else
+        α = 0
+    end
+
+    ϕ̂ = 1/C*[-mNah*hNah*(V-VNa);
+            -mKdh*(V-VK);
+            -mKCainf(Cah,half_acts[12])*(V-VK);
+            -mCaLh * (V-VCa);
+            -mCaTh * hCaTh * (V-VCa)]
+
+    bh = (1/C) * (
+    # Potassium Currents
+    -gAf*mAfh*hAfh*(V-VK) -gAs*mAsh*hAsh*(V-VK) +
+    # Cation current
+    -gH*mHh*(V-VH) +
+    # Passive currents
+    -gl*(V-Vl) +
+    # Stimulation currents
+    +Iapp(t) + I1*pulse(t,ti1,tf1) + I2*pulse(t,ti2,tf2))
+
+    # NOTE: No transpose in Psi as everything is 1D.
+
+    # dV^
+    du[14] = dot(ϕ̂',θ̂) + bh + γ0 + sum((γs .* Ψ .* P .* Ψ))*(V-Vh)
+
+    # Internal dynamics
+    du[15] = (1/tau_mNa(V,ha_ts[1])) * (mNainf(V,half_acts[1]) - mNah)
+    du[16] = (1/tau_hNa(V,ha_ts[2])) * (hNainf(V,half_acts[2]) - hNah)
+    du[17] = (1/tau_mKd(V,ha_ts[3])) * (mKdinf(V,half_acts[3]) - mKdh)
+    du[18] = (1/tau_mAf(V,ha_ts[4])) * (mAfinf(V,half_acts[4]) - mAfh)
+    du[19] = (1/tau_hAf(V,ha_ts[5])) * (hAfinf(V,half_acts[5]) - hAfh)
+    du[20] = (1/tau_mAs(V,ha_ts[6])) * (mAsinf(V,half_acts[6]) - mAsh)
+    du[21] = (1/tau_hAs(V,ha_ts[7])) * (hAsinf(V,half_acts[7]) - hAsh)
+    du[22] = (1/tau_mCaL(V,ha_ts[8])) * (mCaLinf(V,half_acts[8]) - mCaLh)
+    du[23] = (1/tau_mCaT(V,ha_ts[9])) * (mCaTinf(V,half_acts[9]) - mCaTh)
+    du[24] = (1/tau_hCaT(V,ha_ts[10])) * (hCaTinf(V,half_acts[10]) - hCaTh)
+    du[25] = (1/tau_mH(V,ha_ts[11])) * (mHinf(V,half_acts[11]) - mHh)
+    du[26] = (1/tau_Ca) * ((-(αCa)*θ̂[4]*mCaLh*(V-VCa))+(-(β)*θ̂[5]*mCaTh*hCaTh*(V-VCa)) - Cah) 
+
+    du[27:31]= γs .* P .* Ψ * (V-Vh); # dθ̂ 
+    du[31+5+1:31+5+5] = -γs .* Ψ .+ ϕ̂;  # dΨ
+    du[31+1:31+5] = α*P .- α*P.*Ψ.*Ψ.*P # dP
+end
