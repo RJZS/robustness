be496fb7f1d0751905895d9c805fc7ce16531e3b
Raphael Schmetterling <rjzs2@cam.ac.uk>
2022-06-17 11:48:59 +0100

    LR II raster first attempt.

---
+++ LR_relfigs_II_gen.jl
@@ -0,0 +1,177 @@
+## Generate the data for the raster plots for the reliability experiment
+# on Luka's bursting circuit.
+
+# Reliability experiments on Luka's circuit
+
+using Plots, Random, Distributions
+using DifferentialEquations, LinearAlgebra, JLD
+
+include("LR_odes.jl")
+# Need to put a seed here?
+
+num_trials = 3
+
+max_error = 0.1 # 0.1 gives a mismatch of up to +/- 5%
+
+d = Normal(0,1)
+noise_sf = 15
+
+## Definition of parameters
+tau_s = 50
+tau_us = 50*50
+
+afn = -2
+asp = 2
+asn =  -1.5
+ausp =  1.5
+
+# If you change these or other deltas, remember to change 'delta_ests'.
+dfn = 0
+dsp = 0
+dsn = -0.88
+dusp = -0.88
+
+afn2 = -2
+asp2 = 2
+asn2 =  -1.7
+ausp2 =  1.5
+
+asyn21 = -0.2
+asyn12 = -0.2
+
+deltasyn = -1
+delta_h = -0.5
+beta = 2
+
+# If you change this, make sure to also change 'delta_ests'
+delta_ests_true = [dfn,dsp,dsn,dusp,delta_h,dfn,dsp,dsn,dusp,delta_h,deltasyn,deltasyn]
+
+delta_h = -0.5
+beta = 2
+
+
+Tfinalrel = 20000
+dt = 0.1
+
+# Noise-generated current
+noise = rand(d, round(Int, Tfinalrel/dt+1))*noise_sf
+Iconst = -2
+
+for i in eachindex(noise)
+    i == 1 ? noise[i] = 0 : noise[i]=noise[i-1]+(noise[i]-noise[i-1])/2000
+end
+noise = noise .+ Iconst
+
+Iapp2 = -0.65
+
+# Initialise arrays which will later be saved as .jld files.
+delta_est_values = zeros(12,num_trials)
+
+Ref = zeros(2,Int(Tfinalrel/dt+2))
+Mis = zeros(2,Int(Tfinalrel/dt+2),num_trials)
+Learned = zeros(2,Int(Tfinalrel/dt+2),num_trials)
+
+thetalearned = zeros(10,num_trials)
+
+# Run the reference simulation.
+Tfinal= Tfinalrel
+tspan=(0.0,Tfinal)
+x0 = [-1.6 -1.6 -1.6 0 0 0]
+u0 = x0
+p=(afn,asp,asn,ausp,afn2,asp2,asn2,ausp2,tau_s,tau_us,noise,Iapp2,asyn21,asyn12,delta_ests_true,beta)
+probRef = ODEProblem(LR_ODE_rel_II!,u0,tspan,p) # Simulation without noise (ODE)
+solRef = solve(probRef,Euler(),adaptive=false,dt=dt)
+t = solRef.t
+Ref[1,:] = solRef[1,:]
+Ref[2,:] = solRef[4,:]
+
+for idx in 1:num_trials
+    println("Trial Number: $idx")
+    mis = (rand(Uniform(0,max_error),12).-max_error/2).+1
+    delta_ests = [0.01,-0.01,-0.88,-0.88,-0.5,-0.01,0.01,-0.88,-0.88,-0.5,-1,-1].*mis
+    global delta_est_values[:,idx] = delta_ests
+
+    # Simulate the mismatch neuron, before learning.
+    Tfinal= Tfinalrel
+    tspan=(0.0,Tfinal)
+    x0 = [-1.5 -1.5 -1.5 -0.5 -0.5 -0.5]
+    u0 = x0
+    p=(afn,asp,asn,ausp,afn2,asp2,asn2,ausp2,tau_s,tau_us,noise,Iapp2,asyn21,asyn12,delta_ests,beta)
+    probMis = ODEProblem(LR_ODE_rel_II!,u0,tspan,p)
+    solMis = solve(probMis,Euler(),adaptive=false,dt=dt)
+    global Mis[1,:,idx] = solMis[1,:]
+    global Mis[2,:,idx] = solMis[4,:]
+
+    # p1=plot(solRef.t, solRef[1,:])
+    # plot!(solRef.t, solMis[1,:])
+
+
+    # Now run the observer.
+    # Iappobs = t -> -2.6 + 0.4*sin(0.001*t)
+    Iappo = -0.8;
+    Iappo2 = -0.65;
+
+    γ = 2;
+    α = 0.0001;
+    # Tfinal= 65000.0; # Non-diag observer converges faster.
+    Tfinal= 20000.0;
+    tspan=(0.0,Tfinal);
+
+    x0 =  [-1.5 -1.5 -1.5 -0.5 -0.5 -0.5];
+    xh0 = [-1 -1 -1 -1.5 -1.5 -1.5];
+    θ̂₀ = 0.1*ones(10)
+    P₀ = ones(10);
+    Ψ₀ = zeros(10);
+    u0 = [x0 xh0 θ̂₀' P₀' Ψ₀'];
+
+    println("Learning...")
+    p=(afn,asp,asn,ausp,afn2,asp2,asn2,ausp2,tau_s,tau_us,Iappo,Iappo2,
+        asyn21,asyn12,delta_ests_true,delta_ests,beta,α,γ);
+    probObs = ODEProblem(LR_observer_II!,u0,tspan,p) # Simulation without noise (ODE)
+    solObs = solve(probObs,Euler(),adaptive=false,dt=dt)
+    println("Finished learning.")
+
+    # global plt0 = plot(solObs.t, solObs[1,:])
+    # global plt1 = plot(solObs.t, solObs[7,:])
+    # plot!(solObs.t, -solObs[8,:])
+    # global plt2 = plot(solObs.t, solObs[9,:])
+
+    # Truncated figures
+    j = size(solObs)[3]
+    # i = round(Int,1*j/5)
+    # plot(solObs.t[i:j], solObs[7,i:j])
+    # plot!(solObs.t[i:j], -solObs[8,i:j]) # Plotting the negative so can compare!
+
+    # Learned parameters
+    afnl = mean(solObs[13,j-10000:j]);
+    aspl = mean(solObs[14,j-10000:j]);
+    asnl = mean(solObs[15,j-10000:j]);
+    auspl = mean(solObs[16,j-10000:j]);
+    asyn12l = mean(solObs[17,j-10000:j]);
+
+    afn2l = mean(solObs[18,j-10000:j]);
+    asp2l = mean(solObs[19,j-10000:j]);
+    asn2l = mean(solObs[20,j-10000:j]);
+    ausp2l = mean(solObs[21,j-10000:j]);
+    asyn21l = mean(solObs[22,j-10000:j]);
+    global thetalearned[:,idx] = [afnl aspl asnl auspl asyn12l afn2l asp2l asn2l ausp2l asyn21l];
+
+
+    # Finally, apply the learned parameters!
+    x0 = [-1.5 -1.5 -1.5 -0.5 -0.5 -0.5]
+    u0 = x0
+    Tfinal= Tfinalrel
+    tspan=(0.0,Tfinal)
+    p=(afnl,aspl,asnl,auspl,afn2l,asp2l,asn2l,ausp2l,tau_s,tau_us,noise,Iapp2,asyn21l,asyn12l,delta_ests,beta)
+    probLearned = ODEProblem(LR_ODE_rel_II!,u0,tspan,p)  # Simulation without noise (ODE)
+    solLearned = solve(probLearned,Euler(),adaptive=false,dt=dt)
+    global Learned[1,:,idx] = solLearned[1,:]
+    global Learned[2,:,idx] = solLearned[4,:]
+
+    # p2=plot(solRef.t, solRef[1,:])
+    # plot!(solLearned.t, solLearned[1,:])
+end
+
+save("sec4_LR_II.jld","noise",noise,
+    "delta_est_values",delta_est_values,"thetalearned",thetalearned,
+    "t",t,"Ref",Ref,"Mis",Mis,"Learned",Learned)
---
+++ LR_relfigs_II_process.py
@@ -0,0 +1,71 @@
+## Generate the raster plots for the reliability experiment
+# on Luka's bursting circuit, using data generated in 'LR_relfigs_burst_gen.jl'.
+
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+
+fname = "sec4_LR_II.jld"
+
+f = h5py.File(fname, "r")
+
+noise     = f.get("noise")
+delta_est_values = f.get("delta_est_values")
+thetalearned     = f.get("thetalearned")
+
+t      = f.get("t")
+Ref     = f.get("Ref")
+Mis     = f.get("Mis")
+Learned = f.get("Learned")
+
+t=np.array(t); Ref=np.array(Ref); Mis=np.array(Mis); Learned=np.array(Learned)
+num_trials = Mis.shape[0]
+
+def convert_to_timings(t, y, thresh=0):
+    event_idxs = np.where(y>thresh)
+    event_times = t[event_idxs]
+    # Use np.diffs to eliminate all but the first spike in each burst.
+    return event_times
+
+# Neuron 1 events
+RefEvents1 = convert_to_timings(t,Ref[:,0])
+PrelearnEvents1     = []
+MisEvents1     = [] # Not used.
+LearnedEvents1 = []
+for i in range(num_trials):
+    PrelearnEvents1.append(convert_to_timings(t,Mis[i,:,0]))
+    MisEvents1.append(convert_to_timings(t,Mis[i,:,0]))
+    LearnedEvents1.append(convert_to_timings(t,Learned[i,:,0]))
+
+PrelearnEvents1.append(RefEvents1)
+
+# Neuron 2 events
+RefEvents2 = convert_to_timings(t,Ref[:,1])
+PrelearnEvents2     = []
+MisEvents2     = [] # Not used.
+LearnedEvents2 = []
+for i in range(num_trials):
+    PrelearnEvents2.append(convert_to_timings(t,Mis[i,:,1]))
+    MisEvents2.append(convert_to_timings(t,Mis[i,:,1]))
+    LearnedEvents2.append(convert_to_timings(t,Learned[i,:,1]))
+
+PrelearnEvents2.append(RefEvents2)
+
+# Colouring of plots
+from itertools import repeat
+mis_color = 'b'
+color_list = []
+color_list.extend(repeat(mis_color,num_trials))
+color_list.append('r') # Ref colour
+
+# plt.eventplot(MisEvents)
+fig, ax = plt.subplots(1,1)
+l1 = ax.eventplot(PrelearnEvents1,colors=color_list)
+l2 = ax.eventplot(LearnedEvents1, colors='g', linelengths=0.8)
+
+fig2, ax2 = plt.subplots(1,1)
+l1 = ax2.eventplot(PrelearnEvents2,colors=color_list)
+l2 = ax2.eventplot(LearnedEvents2, colors='g', linelengths=0.8)
+
+plt.figure(); plt.plot(t,Mis[0,:,0],t,Mis[0,:,1])
+plt.show()
