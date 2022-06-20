03cc7a4adfedb9970fa1c40057343d61023a8646
Raphael Schmetterling <rjzs2@cam.ac.uk>
2022-06-20 17:20:01 +0100

    Sec2 LR II with modulation.

--- LR_odes.jl
+++ LR_odes.jl
@@ -262,6 +262,89 @@ function LR_II_ODE_with_inact!(du,u,p,t)
     du[6] = (1/tau_us) * (V2 - Vus2)
 end
 
+# Version of II circuit with time-varying gains.
+function LR_II_ODE_with_inact_mod!(du,u,p,t)
+    # Gains
+    afn = p[1]
+    asp = p[2]
+    asn = p[3]
+    ausp = p[4]
+
+    # Offsets
+    dfn = p[5]
+    dsp = p[6]
+    dsn = p[7]
+    dusp = p[8]
+
+    # Time constants
+    tau_s = p[9]
+    tau_us = p[10]
+
+    # Input
+    Iapp = p[11]
+    I1=p[12] # Amplitude of first step input
+    I2=p[13] # Amplitude of second step input
+    ti1=p[14] # Starting time of first step input
+    tf1=p[15] # Ending time of first step input
+    ti2=p[16] # Starting time of second step input
+    tf2=p[17] # Ending time of second step input
+
+    # Gains of second neuron
+    afn2 = p[18]
+    asp2 = p[19]
+    asn2 = p[20]
+    ausp2 = p[21]
+
+    # Input to second neuron
+    Iapp2 = p[22]
+
+    # Synaptic parameters
+    asyn21 = p[23]
+    asyn12 = p[24]
+
+    deltasyn = p[25]
+    beta = 2
+
+    I3 = p[26]
+    ti3 = p[27]
+    tf3 = p[28]
+
+    delta_h = p[29]
+
+    I4 = p[30]
+    ti4 = p[31]
+    tf4 = p[32]
+    I5 = p[33]
+    ti5 = p[34]
+    tf5 = p[35]
+
+    # Variables
+    V = u[1]
+    Vs = u[2]
+    Vus = u[3]
+
+    V2 = u[4]
+    Vs2 = u[5]
+    Vus2 = u[6]
+
+    # ODEs
+    asn_with_inact = asn(t) * sigmoid(-(Vus-delta_h),beta)
+    du[1] = -V  -element(V,afn(t),dfn) -element(Vs,asp(t),dsp) +
+                -element(Vs,asn_with_inact,dsn) -element(Vus,ausp(t),dusp) +
+                synapse(Vs2, asyn12(t), deltasyn, beta) +
+                Iapp + I1*pulse(t,ti1,tf1) + I2*pulse(t,ti2,tf2)
+    du[2] = (1/tau_s)  * (V - Vs)
+    du[3] = (1/tau_us) * (V - Vus)
+
+    asn2_with_inact = asn2(t) * sigmoid(-(Vus2-delta_h),beta)
+    du[4] = -V2  -element(V2,afn2(t),dfn) -element(Vs2,asp2(t),dsp) +
+                -element(Vs2,asn2_with_inact,dsn) -element(Vus2,ausp2(t),dusp) +
+                synapse(Vs, asyn21(t), deltasyn, beta) +
+                Iapp2 + I3*pulse(t,ti3,tf3) + I4*pulse(t,ti4,tf4) + I5*pulse(t,ti5,tf5)
+    du[5] = (1/tau_s)  * (V2 - Vs2)
+    du[6] = (1/tau_us) * (V2 - Vus2)
+end
+
 # For running the reliability experiment.
 function LR_ODE_rel!(du,u,p,t)
     # Gains
